cd310313c3feaa45b4dd6abba074a78f3306b681
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-25 00:11:05 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
